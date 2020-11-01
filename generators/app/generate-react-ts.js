1c6acaba62a873cb80fc4d94802170ca59a79dcd
José León <jose.leon@branchcreation.com>
2020-11-01 10:42:08 +0100

    Updated to include the first react generator

---
+++ generators/app/generate-react-ts.js
@@ -0,0 +1,49 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+const prompts = require("./prompts");
+
+module.exports = {
+    id: 'ext-react-ts',
+    name: 'New React Web Application (TypeScript)',
+    /**
+     * @param {import('yeoman-generator')} generator
+     * @param {Object} extensionConfig
+     */
+    prompting: async (generator, extensionConfig) => {
+        await prompts.askForExtensionId(generator, extensionConfig);
+
+        await prompts.askForGit(generator, extensionConfig);
+        await prompts.askForPackageManager(generator, extensionConfig);
+
+
+
+    },
+    /**
+     * @param {import('yeoman-generator')} generator
+     * @param {Object} extensionConfig
+     */
+    writing: (generator, extensionConfig) => {
+        generator.fs.copy(generator.sourceRoot() + '/public', 'public');
+        generator.fs.copy(generator.sourceRoot() + '/src', 'src');
+
+        if (extensionConfig.gitInit) {
+            generator.fs.copy(generator.sourceRoot() + '/gitignore', '.gitignore');
+        }
+
+        if (extensionConfig.pkgManager === 'yarn') {
+            generator.fs.copyTpl(generator.sourceRoot() + '/README.yarn.md', 'README.md', extensionConfig);
+            generator.fs.copyTpl(generator.sourceRoot() + '/yarn.lock', 'yarn.lock', extensionConfig);
+        }
+        else {
+            generator.fs.copyTpl(generator.sourceRoot() + '/README.md', 'README.md', extensionConfig);
+            generator.fs.copyTpl(generator.sourceRoot() + '/package-lock.json', 'package-lock.json', extensionConfig);
+
+        }
+        generator.fs.copyTpl(generator.sourceRoot() + '/package.json', 'package.json', extensionConfig);
+        generator.fs.copyTpl(generator.sourceRoot() + '/tsconfig.json', 'tsconfig.json', extensionConfig);
+
+        extensionConfig.installDependencies = true;
+    }
+}
--- generators/app/index.js
+++ generators/app/index.js
@@ -9,17 +9,7 @@ const yosay = require('yosay');
 const path = require('path');
 const env = require('./env');
 
-const colortheme = require('./generate-colortheme');
-const commandjs = require('./generate-command-js');
-const commandts = require('./generate-command-ts ');
-const commandweb = require('./generate-command-web');
-const extensionpack = require('./generate-extensionpack');
-const keymap = require('./generate-keymap');
-const language = require('./generate-language');
-const localization = require('./generate-localization');
-const notebook = require('./generate-notebook-renderer');
-const snippets = require('./generate-snippets');
-const webupdate = require('./generate-web-update');
+const reactts = require('./generate-react-ts');
 
 module.exports = class extends Generator {
 
@@ -27,12 +17,6 @@ module.exports = class extends Generator {
         super(args, opts);
         this.option('extensionType', { type: String });
         this.option('extensionName', { type: String });
-        this.option('extensionDescription', { type: String });
-        this.option('extensionDisplayName', { type: String });
-
-        this.option('extensionParam', { type: String });
-        this.option('extensionParam2', { type: String });
-        this.option('insiders', { type: Boolean, alias: 'i' });
 
         this.extensionConfig = Object.create(null);
         this.extensionConfig.installDependencies = false;
@@ -44,32 +28,14 @@ module.exports = class extends Generator {
 
     async initializing() {
         const cliArgs = this.options['_'];
-        this.extensionConfig.insiders = Array.isArray(cliArgs) && cliArgs.indexOf('insiders') !== -1 || !!this.options['insiders'];
 
         // Welcome
-        if (!this.extensionConfig.insiders) {
-            this.log(yosay('Welcome to the Visual Studio Code Extension generator!'));
-        } else {
-            this.log(yosay('Welcome to the Visual Studio Code Insiders Extension generator!'));
-        }
-
-        // evaluateEngineVersion
-        const dependencyVersions = await env.getDependencyVersions();
-        this.extensionConfig.dependencyVersions = dependencyVersions;
-        this.extensionConfig.dep = function (name) {
-            const version = dependencyVersions[name];
-            if (typeof version === 'undefined') {
-                throw new Error(`Module ${name} is not listed in env.js`);
-            }
-            return `${JSON.stringify(name)}: ${JSON.stringify(version)}`;
-        };
-        this.extensionConfig.vsCodeEngine = await env.getLatestVSCodeVersion();
+        this.log(yosay('Welcome to the React generator!'));
     }
 
     async prompting() {
         const extensionGenerators = [
-            commandts, commandjs, colortheme, language, snippets, keymap, extensionpack, localization,
-            commandweb, notebook, webupdate
+            reactts
         ]
 
         // Ask for extension type
@@ -123,9 +89,6 @@ module.exports = class extends Generator {
         if (this.abort) {
             return;
         }
-        if (!this.extensionGenerator.update) {
-            process.chdir(this.extensionConfig.name);
-        }
         if (this.extensionConfig.installDependencies) {
             this.installDependencies({
                 yarn: this.extensionConfig.pkgManager === 'yarn',
@@ -141,64 +104,30 @@ module.exports = class extends Generator {
             return;
         }
 
-        if (this.extensionGenerator.update) {
-            this.log('');
-            this.log('Your extension has been updated!');
-            this.log('');
-            this.log('To start editing with Visual Studio Code, use the following commands:');
-            this.log('');
-            if (!this.extensionConfig.insiders) {
-                this.log('     code .');
-            } else {
-                this.log('     code-insiders .');
-            }
-            this.log(`     ${this.extensionConfig.pkgManager} run compile-web`);
-            this.log('');
-            return;
-        }
-
         // Git init
         if (this.extensionConfig.gitInit) {
             this.spawnCommand('git', ['init', '--quiet']);
         }
 
-        if (this.extensionConfig.proposedAPI) {
-            this.spawnCommand(this.extensionConfig.pkgManager, ['run', 'update-proposed-api']);
-        }
         this.log('');
 
-        if (!this.extensionConfig.insiders) {
-            this.log('Your extension ' + this.extensionConfig.name + ' has been created!');
-            this.log('');
-            this.log('To start editing with Visual Studio Code, use the following commands:');
-            this.log('');
-            this.log('     cd ' + this.extensionConfig.name);
-            this.log('     code .');
-            this.log('');
-            this.log('Open vsc-extension-quickstart.md inside the new extension for further instructions');
-            this.log('on how to modify, test and publish your extension.');
-        } else {
-            this.log('Your extension ' + this.extensionConfig.name + ' has been created!');
-            this.log('');
-            this.log('To start editing with Visual Studio Code, use the following commands:');
-            this.log('');
-            this.log('     cd ' + this.extensionConfig.name);
-            this.log('     code-insiders .');
-            this.log('');
-            this.log('Open vsc-extension-quickstart.md inside the new extension for further instructions');
-            this.log('on how to modify and test your extension.');
-        }
+        //TODO: Dump the README.md to show the commands
+        this.log('Your extension ' + this.extensionConfig.name + ' has been created!');
+        this.log('');
+        this.log('To start editing with Visual Studio Code, use the following commands:');
+        this.log('');
+        this.log('     cd ' + this.extensionConfig.name);
+        this.log('     code .');
+        this.log('');
+        this.log('Open vsc-extension-quickstart.md inside the new extension for further instructions');
+        this.log('on how to modify, test and publish your extension.');
+
         this.log('');
 
         if (this.extensionGenerator.endMessage) {
             this.extensionGenerator.endMessage(this, this.extensionConfig);
         }
 
-        this.log('For more information, also visit http://code.visualstudio.com and follow us @code.');
         this.log('\r\n');
-
-        if (this.extensionConfig.insiders) {
-            this.spawnCommand('code-insiders', [this.destinationPath(this.extensionConfig.name)]);
-        }
     }
 }
